e0a0f6b4e551e15b981211bb843b032343bbbd9c
Alnoman Robin <robinalnoman@gmail.com>
2025-04-21 14:08:50 +0600

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    // Personal Information (as a comment)
+    /*
+    Name: Robin
+    Phone: 01774139359
+    Email: robinalnoman@gmail.com
+    */
+
+    int num1 = 35;
+    float num2 = 77.54329;
+    char ch = 'a';
+    char ch1 = 'r';
+
+    printf("The number1 is: %d\n", num1);
+    printf("The number2 is: %.2f\n", num2);
+    printf("The Character is: %c%c\n", ch, ch1);
+    printf("ROBIN\n");
+
+    return 0; // my name is robin
+}
