58f2753f0b0cc1e0937fc69b73e7cb03e60dfc17
Alnoman Robin <robinalnoman@gmail.com>
2025-04-22 23:08:22 +0600

    take three number

---
+++ 08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()//take three number
+{
+    int num1,num2,num3;
+    printf("Inter first number:");
+    scanf("%d",&num1);
+    printf("Inter second number:");
+    scanf("%d",&num2);
+    printf("Inter Third number:");
+    scanf("%d",&num3);
+    printf("The number are:\n%d\n%d\n%d",num1,num2,num3);
+return 0;
+}
