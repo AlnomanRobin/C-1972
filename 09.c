0a676bcb7bfe46f1e791141353bab29213215630
Alnoman Robin <robinalnoman@gmail.com>
2025-04-24 02:39:11 +0600

    take two floating number

---
+++ 09.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()//take two floating number
+{
+float num1,num2;
+printf("Inter first number:");
+scanf("%f",&num1);
+printf("Inter second number:");
+scanf("%f",&num2);
+printf("The first number is:%f\n",num1);
+printf("The second number is:%f\n",num2);
+return 0;
+}
