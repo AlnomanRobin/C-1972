04ae1094a57319024736c2d40aced72b5028c991
Alnoman Robin <robinalnoman@gmail.com>
2025-04-24 02:39:49 +0600

    Type Declaration Instructions

---
+++ 10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()//take two number and his summation
+{
+    int A,B;
+    printf("the first number:");
+    scanf("%d",&A);
+    printf("the second number:");
+    scanf("%d",&B);
+    int sum;
+    sum=A+B;
+    printf("The sum is:%d",sum);
+    return 0;
+}
