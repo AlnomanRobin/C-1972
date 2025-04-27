583159a4c30fe23f89400a0fe2466f00b79a17e6
Alnoman Robin <robinalnoman@gmail.com>
2025-04-28 02:19:11 +0600

    any two number and gets sum, sub, multipli, div, mod, power.

---
+++ 14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()//take two number and his Division
+{
+int num1,num2;
+printf("The first number is:");
+scanf("%d",&num1);
+printf("The second number is:");
+scanf("%d",&num2);
+int div;
+div=num1/num2;
+printf("the division is:%d",div);
+return 0;
+}
