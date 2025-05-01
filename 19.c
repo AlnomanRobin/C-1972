5cd7afdd73f752977d49bdcfb9b0392f70565249
Alnoman Robin <robinalnoman@gmail.com>
2025-05-01 17:13:26 +0600

    relational operators(true=1 and false=0)

---
+++ 19.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()//relational operators(true=1 and false=0)
+{
+printf("%d\n",4>3);
+printf("%d\n",3>3);//fals
+printf("%d\n",3>=3);//true
+printf("%d\n",4!=4);// !=not
+printf("%d\n",4==4);
+printf("%d\n",4!=3);
+return 0;
+}
