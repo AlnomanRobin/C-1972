ff6b64c09bf1586431ba06b782aa4c27064ab75c
Alnoman Robin <robinalnoman@gmail.com>
2025-05-02 20:24:54 +0600

    Write a program to cheak if a number is divisible by 2 or not

---
+++ 22.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()// 2 dara bivajoo kina jachay;
+//Write a program to cheak if a number is divisible by 2 or not.
+
+{
+    int x;
+printf("Enter a number:");
+scanf("%d",&x);
+printf("%d",x%2==0);
+return 0;
+}
+
