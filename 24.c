23b0dc0f111df21bd2d8b0262a37d36dea17eb50
Alnoman Robin <robinalnoman@gmail.com>
2025-05-05 19:15:32 +0600

    if a number is greater then 9 & less than 100->True

---
+++ 24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()//if a number is greater then 9 & less than 100->True
+{
+int x;
+printf("Enter number:");
+scanf("%d",&x);
+printf("%d\n",x>9&&x<100);
+return 0;
+}
