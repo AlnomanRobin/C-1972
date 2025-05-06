f144f77b5fbd5d9b3b25e9142183b3bd3f8f8831
Alnoman Robin <robinalnoman@gmail.com>
2025-05-06 22:14:15 +0600

    Write a program to print the average of 3 number.

---
+++ 25.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()//Write a program to print the average of 3 number.
+
+{
+int a,b,c;
+scanf("%d\n%d\n%d",&a,&b,&c);
+//scanf("\n%d",&b);
+//scanf("\n%d",&c);
+int Av=(a+b+c)/3;
+printf("The average is:%d",Av);
+return 0;
+}
