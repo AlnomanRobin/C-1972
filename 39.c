15e44dc71de8bad7baa097f72ebe43a44e7b8058
Alnoman Robin <robinalnoman@gmail.com>
2025-05-21 00:25:45 +0600

    MEMORY FULL

---
+++ 39.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()/*
+            ++i(pre increment)
+            i++(post increment)
+            --i(pre decrement)
+            i--(post decrement)
+            */
+{
+    int i=1;
+    //printf("%d \n",i--);
+    //printf("%d \n",i);
+    printf("%d \n",--i);
+    printf("%d \n",i);
+    return 0;
+}
