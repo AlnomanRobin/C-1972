9375b5c57ef8b02c8214273a31148ea5c1e66f9c
Alnoman Robin <robinalnoman@gmail.com>
2025-05-25 20:49:14 +0600

    sum of first n natural numbers.(5=1+2+3+4+5=15)

---
+++ 45.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()//print the sum of first n natural numbers.(5=1+2+3+4+5=15)
+{
+int n;
+printf("%d",n);
+scanf("%d",&n);
+int sum=0;
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;//sum+=i
+}
+printf("sum is %d\n",sum);
+return 0;
+}
