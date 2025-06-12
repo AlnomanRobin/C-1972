5c2070d60c273fd6c2d722c8db90afb0b278327c
Alnoman Robin <robinalnoman@gmail.com>
2025-06-12 15:19:23 +0600

    Function

---
+++ 57.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//Declaration /Prototype
+void printHello();
+int main()
+{
+    printHello();//Function call.
+    printHello();
+    printHello();
+    return 0;
+}
+//Function definition
+void printHello()
+{
+    print("Hello!\n");
+}
