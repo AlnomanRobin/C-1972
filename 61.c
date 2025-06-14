98c83253ddb4dc48474f7b0adfedc893a59a1234
Alnoman Robin <robinalnoman@gmail.com>
2025-06-14 17:01:32 +0600

    Star print

---
+++ 61.c
@@ -0,0 +1,15 @@
+//Star print
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<=10;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
